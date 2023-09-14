b15c12b00245ac43c0681047881716c91a8b18cf
Mackenzie Bishop <69914681+mackenziebishop@users.noreply.github.com>
2023-09-14 12:17:40 -0500

    Completed ch10lab1

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/*
+  Student Name: Mackenzie Bishop
+  File Name: script.js
+  Date: September 14, 2023
+*/
+
+//Hamburger menu function
+function hamburger(){
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display == "block") {
+        menu.style.display == "none";
+    } else {
+        menu.style.display == "block";
+    }
+}
